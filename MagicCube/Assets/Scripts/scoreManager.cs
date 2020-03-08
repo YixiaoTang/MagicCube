1138d4b40a14dddd5c0377f2cc20256b3ef08f96
anidevo <ani-devoian@hotmail.com>
2020-03-08 11:22:53 -0700

    added score tracker

---
+++ MagicCube/Assets/Scripts/scoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreManager : MonoBehaviour
+{
+    public static float scoreTotal;
+    public float highScore;
+    GameObject[] players;
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    void Update()
+    {
+        scoreTotal = GM.ballSize * 2.5f;
+    }
+}
--- MagicCube/Assets/Scripts/status.cs
+++ MagicCube/Assets/Scripts/status.cs
@@ -7,10 +7,26 @@ public class status : MonoBehaviour
 {
     [SerializeField] Text coinText;
     [SerializeField] Text timer;
+
+    [SerializeField] Text score1;
+    [SerializeField] Text score2;
+    [SerializeField] Text score3;
+
+    [SerializeField] Text player1;
+    [SerializeField] Text player2;
+    [SerializeField] Text player3;
     // 显示分数
     void Update()
     {
         coinText.text = "" + GM.coinTotal;
         timer.text = "" + GM.counterTotal;
+        score1.text = "" + scoreManager.scoreTotal;
+        score2.text = "" + scoreManager.scoreTotal;
+        score3.text = "" + scoreManager.scoreTotal;
+
+        player1.text = "1." + GameObject.Find("Player");
+        player2.text = "1." + GameObject.Find("Player");
+        player3.text = "1." + GameObject.Find("Player");
+
     }
 }
